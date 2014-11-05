bbf463d8e5b7802b161b4c787ed932c08b4483e0
Nathan Vander Wilt <nate@calftrail.com>
2014-11-05 13:56:34 -0800

    couple tweaks to make push tarball handling reliable even on fresh checkouts/any repo folder name

--- push.sh
+++ push.sh
@@ -4,11 +4,11 @@ set -o nounset -o errexit
 
 : ${TESSEL_KEY:?}
 : ${TESSEL_ID:=`tessel list`}
-: ${TAR_FILE:=tessel-tmp.tar}
+: ${TAR_FILE:=tessel-${PWD##*/}.tar}
 : ${NUM_FILE:=the_number}
 
 echo $((`cat $NUM_FILE` + 1)) > $NUM_FILE   # increment counter
-rm $TAR_FILE            # prevent exponential tarball growth ;-)
+rm -f $TAR_FILE            # prevent exponential tarball growth ;-)
 tessel pack index.js
 
 curl -X POST -H "Accept: application/vnd.tessel.remote.v1" -F "api_key=$TESSEL_KEY" -F "device_id=$TESSEL_ID" -F "script_tar=@$TAR_FILE" http://localhost:3000/api/tessel/$TESSEL_ID/code
