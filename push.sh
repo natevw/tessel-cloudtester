4d636d0fd4052eeb865faaef2996ef131c85282b
Nathan Vander Wilt <nate@calftrail.com>
2014-11-05 14:30:49 -0800

    need to push client code each time, since it's not built in to firmware

--- push.sh
+++ push.sh
@@ -4,6 +4,8 @@ set -o nounset -o errexit
 
 : ${TESSEL_KEY:?}
 : ${TESSEL_ID:=`tessel list`}
+: ${NET_ADDR:=`ifconfig | awk '/inet /{print $2}' | tail -n 1`}
+: ${NET_PORT:=3000}
 : ${TAR_FILE:=tessel-${PWD##*/}.tar}
 : ${NUM_FILE:=the_number}
 
@@ -11,5 +13,9 @@ echo $((`cat $NUM_FILE` + 1)) > $NUM_FILE   # increment counter
 rm -f $TAR_FILE            # prevent exponential tarball growth ;-)
 tessel pack index.js
 
-curl -X POST -H "Accept: application/vnd.tessel.remote.v1" -F "api_key=$TESSEL_KEY" -F "device_id=$TESSEL_ID" -F "script_tar=@$TAR_FILE" http://localhost:3000/api/tessel/$TESSEL_ID/code
+# this is needed until firmware includes…
+tessel push ../cloud-client/index.js tessel push --args $NET_ADDR $NET_PORT
+sleep 5   # give script a chance to startup and register itself
+
+curl -X POST -H "Accept: application/vnd.tessel.remote.v1" -F "api_key=$TESSEL_KEY" -F "device_id=$TESSEL_ID" -F "script_tar=@$TAR_FILE" http://$NET_ADDR:$NET_PORT/api/tessel/$TESSEL_ID/code
 echo
